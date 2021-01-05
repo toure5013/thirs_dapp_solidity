d84c5eb0db1c873be5524be7850fcbb7f87c5517
TOURE SOULEYMANE <toure5013@gmail.com>
2021-01-05 10:15:43 +0000

    cntracts in progress

---
+++ election/contracts/Election.sol
@@ -0,0 +1,30 @@
+pragma solidity ^0.5.16;
+
+contract Election{
+	//Model a Candidate 
+	struct Candidate {
+		uint id;
+		string name;
+		uint votCount;
+	}
+	
+	//store Candidates
+	//fetch Candidates
+	mapping (uint => Candidate) public candidates;
+
+	//Store candidates Count
+	uint public candidatesCount;
+
+	constructor() public{
+		addCandidate("Candidate 1");
+		addCandidate("Candidate 2 ");
+	}
+
+
+	function addCandidate (string memory _name) private {
+		candidatesCount++;
+		candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
+	}
+	
+
+}
---
+++ election/test/election.js
@@ -0,0 +1,29 @@
+var Election = artifacts.require("./Election.sol");
+
+contract("Election", function(accounts){
+	var electionInstance;
+	it("initializes with two candidates", function(){
+		return Election.deployed().then(function(instance){
+			return instance.candidatesCount();
+		}).then(function(count){
+			assert.equal(count, 2);
+		});
+	})
+
+
+	it("initializes the candidates with the correct values", function(){
+		return Election.deployed().then(function(instance){
+			electionInstance = instance;
+			return electionInstance.candidates(1);
+		}).then(function(candidate){
+			assert.equal(candidate[0], 1 , "containes the correct id" );
+			assert.equal(candidate[1], "containes the correct id" );
+			assert.equal(candidate[2], 0, "containes the correct id" );
+			return electionInstance.candidates(2);
+		}).then(function(candidate){
+			assert.equal(candidate[0], 2, "containes the correct id" );
+			assert.equal(candidate[1], "containes the correct id" );
+			assert.equal(candidate[2], 0, "containes the correct id" );
+		});
+	});
+});
